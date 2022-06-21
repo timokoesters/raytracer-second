689828579812859dc80cede4c47302d54f84ee66
second2050 <git@second2050.me>
2022-06-21 18:18:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# second2050's Raytracer
+I am doing this as part of the Comutergrapfik AG (CG Workgroup) of my University. 
---
+++ me/second2050/raytracer/RaytracerMain.java
@@ -0,0 +1,59 @@
+package me.second2050.raytracer;
+
+import java.io.*;
+
+class RaytracerMain {
+    // global variables
+    static final int IMAGE_WIDTH = 256;
+    static final int IMAGE_HEIGHT = 256;
+    static final String OUTPUT_FILE_NAME = "output.ppm";
+    public static void main(String[] args) {
+        // open file
+        File file;
+        try {
+            file = new File("./" + OUTPUT_FILE_NAME);
+        } catch (Exception e) {
+            System.out.printf("ERROR: Couldn't open output file: %s\n", OUTPUT_FILE_NAME);
+            e.printStackTrace();
+            System.exit(255);
+            return;
+        }
+        
+        // create file
+        PrintWriter output;
+        try {
+            // delete file if it exists already
+            if (file.exists()) {
+                file.delete();
+            }
+            file.createNewFile();
+            output = new PrintWriter(file);
+        } catch (Exception e) {
+            System.out.printf("ERROR: Couldn't create new output file: %s\n", OUTPUT_FILE_NAME);
+            e.printStackTrace();
+            System.exit(255);
+            return;
+        }
+
+        // render test image
+        output.printf("P3\n%d %d\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT); // file header
+        for (int i = IMAGE_HEIGHT-1; i >= 0; i--) {
+            System.out.printf("Scanlines remaining: %d\n", i);
+            for (int j = 0; j < IMAGE_WIDTH; j++) {
+                double r = (double)j / (IMAGE_WIDTH-1);
+                double g = (double)i / (IMAGE_HEIGHT-1);
+                double b = 0.25;
+
+                int ir = (int)(255.999 * r);
+                int ig = (int)(255.999 * g);
+                int ib = (int)(255.999 * b);
+
+                // System.out.printf("   i, j: %d %d\n", i, j);
+                // System.out.printf("r, g, b: %f %f %f\n", r, g, b);
+                output.printf("%d %d %d\n", ir, ig, ib);
+            }
+        }
+        output.close(); // close file to ensure correct writing to storage
+        return;
+    }
+}
